75c97facada4d77fe15cc54c3b558be9d54a891e
Adrian Samaniego <llathander@gmail.com>
2019-11-11 17:40:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,4 +13,5 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index.map do |attendee, index|
-  "Hello, #{attendee}!"
+  "Hello, #{attendee}!, You'll be assigned to room #{index+1}"
+  
