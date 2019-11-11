e2a22b240da24ecc30d0451b858a85ddd7558f2d
Adrian Samaniego <llathander@gmail.com>
2019-11-11 17:43:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,5 +17,12 @@ def assign_rooms(attendees)
   end
 end
   
-def printer
-  batch_badge_creator
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
